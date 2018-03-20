e46a0d0a629a642f588ca73522bc901ed105e585
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2018-03-19 20:10:31 -0700

    Initial configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAACw7pCYYLcBAMCI7ZAjvq8D3U4PJGenBEGoTUxKPqjJ1PntgzeIcOacEqzLIvPyJThcWvjNQTZAlbaSYTyIdA67EAVWuTFOdJuiPPB2o3vpshSZButJe6NqDTNZAq0njgZCaSotGqFy4p8OFoomTt12aPXj0k3iuOJxcD8RENgZDZD',
+    FB_VERIFY_TOKEN: 'pipofoED03',
+    API_AI_CLIENT_ACCESS_TOKEN: 'f7e82edc02c441d8bc3cdeb985c3d35c',
+    FB_APP_SECRET: '026d6313ec51e28ae7bffc6502a5de94',
+    SERVER_URL: "https://chatboted02.herokuapp.com/",
 };
