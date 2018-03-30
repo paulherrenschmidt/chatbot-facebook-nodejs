1f0fdbbc16c42e28aca9f8155cb41b273e5e8fd5
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2018-03-30 14:36:13 +0200

    chatbasetest

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const request = require('request');
 const app = express();
 const uuid = require('uuid');
 
+const chatbase = require('@google/chatbase')
+  .setApiKey('8e9752cc-8c30-4f28-a55e-a35bc10a0950')  // Your api key
+  .setUserId('some-unique-user-id') // The id of the user you are interacting with
+  .setPlatform('gorge-of-peril') // The platform the bot is interacting on/over
+  .setVersion('1.0') // The version of the bot deployed
+  .setIntent('cross-gorge-of-peril'); // the intent of the user message
+
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
@@ -75,7 +82,7 @@ app.get('/webhook/', function (req, res) {
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
  * webhook. Be sure to subscribe your app to your page to receive callbacks
- * for your page. 
+ * for your page.
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
@@ -166,7 +173,7 @@ function receivedMessage(event) {
 
 function handleMessageAttachments(messageAttachments, senderID){
 	//for now just reply
-	sendTextMessage(senderID, "Attachment received. Thank you.");	
+	sendTextMessage(senderID, "Attachment received. Thank you.");
 }
 
 function handleQuickReply(senderID, quickReply, messageId) {
@@ -184,6 +191,21 @@ function handleEcho(messageId, appId, metadata) {
 
 function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 	switch (action) {
+    case "question":
+      let replies = [
+          {
+            "content_type":"text",
+            "title":"BBA1",
+            "payload":"BBA1"
+          },
+          {
+            "content_type":"text",
+            "title":"BBA2",
+            "payload":"BBA100"
+          }
+      ];
+      sendQuickReply(sender, responseText, replies);
+      break;
 		default:
 			//unhandled action, just send back the text
 			sendTextMessage(sender, responseText);
@@ -193,6 +215,10 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
 function handleMessage(message, sender) {
 	switch (message.type) {
 		case 0: //text
+      chatbase.newMessage()
+        .setMessage(message)
+        .send()
+        .catch(e => console.error(e));
 			sendTextMessage(sender, message.speech);
 			break;
 		case 2: //quick replies
@@ -206,13 +232,21 @@ function handleMessage(message, sender) {
 				}
 				replies.push(reply);
 			}
+      chatbase.newMessage()
+        .setMessage(message)
+        .send()
+        .catch(e => console.error(e));
 			sendQuickReply(sender, message.title, replies);
 			break;
 		case 3: //image
+      chatbase.newMessage()
+        .setMessage(message)
+        .send()
+        .catch(e => console.error(e));
 			sendImageMessage(sender, message.imageUrl);
 			break;
 		case 4:
-			// custom payload
+			// custom payload//
 			var messageData = {
 				recipient: {
 					id: sender
@@ -671,8 +705,8 @@ function greetUserText(userId) {
 }
 
 /*
- * Call the Send API. The message data goes in the body. If successful, we'll 
- * get the message id in a response 
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
  *
  */
 function callSendAPI(messageData) {
@@ -707,17 +741,17 @@ function callSendAPI(messageData) {
 /*
  * Postback Event
  *
- * This event is called when a postback is tapped on a Structured Message. 
+ * This event is called when a postback is tapped on a Structured Message.
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
- * 
+ *
  */
 function receivedPostback(event) {
 	var senderID = event.sender.id;
 	var recipientID = event.recipient.id;
 	var timeOfPostback = event.timestamp;
 
-	// The 'payload' param is a developer-defined field which is set in a postback 
-	// button for Structured Messages. 
+	// The 'payload' param is a developer-defined field which is set in a postback
+	// button for Structured Messages.
 	var payload = event.postback.payload;
 
 	switch (payload) {
@@ -739,7 +773,7 @@ function receivedPostback(event) {
  *
  * This event is called when a previously-sent message has been read.
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-read
- * 
+ *
  */
 function receivedMessageRead(event) {
 	var senderID = event.sender.id;
@@ -759,7 +793,7 @@ function receivedMessageRead(event) {
  * This event is called when the Link Account or UnLink Account action has been
  * tapped.
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/account-linking
- * 
+ *
  */
 function receivedAccountLink(event) {
 	var senderID = event.sender.id;
@@ -775,7 +809,7 @@ function receivedAccountLink(event) {
 /*
  * Delivery Confirmation Event
  *
- * This event is sent to confirm the delivery of a message. Read more about 
+ * This event is sent to confirm the delivery of a message. Read more about
  * these fields at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-delivered
  *
  */
@@ -800,8 +834,8 @@ function receivedDeliveryConfirmation(event) {
 /*
  * Authorization Event
  *
- * The value for 'optin.ref' is defined in the entry point. For the "Send to 
- * Messenger" plugin, it is the 'data-ref' field. Read more at 
+ * The value for 'optin.ref' is defined in the entry point. For the "Send to
+ * Messenger" plugin, it is the 'data-ref' field. Read more at
  * https://developers.facebook.com/docs/messenger-platform/webhook-reference/authentication
  *
  */
@@ -811,9 +845,9 @@ function receivedAuthentication(event) {
 	var timeOfAuth = event.timestamp;
 
 	// The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
-	// The developer can set this to an arbitrary value to associate the 
+	// The developer can set this to an arbitrary value to associate the
 	// authentication callback with the 'Send to Messenger' click event. This is
-	// a way to do account linking when the user clicks the 'Send to Messenger' 
+	// a way to do account linking when the user clicks the 'Send to Messenger'
 	// plugin.
 	var passThroughParam = event.optin.ref;
 
@@ -827,8 +861,8 @@ function receivedAuthentication(event) {
 }
 
 /*
- * Verify that the callback came from Facebook. Using the App Secret from 
- * the App Dashboard, we can verify the signature that is sent with each 
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
  * callback in the x-hub-signature field, located in the header.
  *
  * https://developers.facebook.com/docs/graph-api/webhooks#setup
