eacca7c3dc3d2e739a49964682a84302f208361c
Paul HERRENSCHMIDT <herrenschmidtpaul@gmail.com>
2018-03-30 14:56:27 +0200

    chatbasetest4

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 const uuid = require('uuid');
-
+const chatbase = require('@google/chatbase')
+  .setApiKey('[your api key]')  // Your api key
+  .setUserId('some-unique-user-id') // The id of the user you are interacting with
+  .setPlatform('gorge-of-peril') // The platform the bot is interacting on/over
+  .setVersion('1.0') // The version of the bot deployed
+  .setIntent('cross-gorge-of-peril'); // the intent of the user message
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
